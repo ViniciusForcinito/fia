0de0fdc26a778490c95d0f1663fc1d109d19e3c6
ViniciusForcinito <viniciusforcinito@gmail.com>
2023-03-16 21:57:32 -0300

    sexto  commit codigo

--- Aula2023-03-09-SparkNaPratica.py
+++ Aula2023-03-09-SparkNaPratica.py
@@ -1,3 +1,4 @@
+#Arquivo modificado
 # Databricks notebook source
 # DBTITLE 1,Execução única
 # Rodar este notebook apenas uma vez, a tabela ficará disponível pra sempre na sua conta do Databricks Community
